1a97e899d7fd3bf2d4b39f408dc4b89b43197ea1
Krishna Anand <krishnanand86654@gmail.com>
2025-02-20 04:36:45 +0530

    Initial commit for gh-pages branch

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC8yZewXbzpgr2cG46yuZQFFfIjiGCNhPw",
+  authDomain: "blog-50ae5.firebaseapp.com",
+  projectId: "blog-50ae5",
+  storageBucket: "blog-50ae5.firebasestorage.app",
+  messagingSenderId: "198332093215",
+  appId: "1:198332093215:web:d20a5710c26c8e4321162a",
+  measurementId: "G-D1ZHDVJT2G",
+};
