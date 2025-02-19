de9fd1c1cd36492868e1c03dfea5322234c1f2ed
Krishna Anand <krishnanand86654@gmail.com>
2025-02-20 04:34:12 +0530

    Updated js

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-// import firebaseConfig from "./firebaseConfig.js";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC8yZewXbzpgr2cG46yuZQFFfIjiGCNhPw",
-//   authDomain: "blog-50ae5.firebaseapp.com",
-//   projectId: "blog-50ae5",
-//   storageBucket: "blog-50ae5.firebasestorage.app",
-//   messagingSenderId: "198332093215",
-//   appId: "1:198332093215:web:d20a5710c26c8e4321162a",
-//   measurementId: "G-D1ZHDVJT2G"
-// };
-
+//API Key for firestore is stored in firebaseCongig.js file
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
